e97cd9720fd03b48c3ebbd14a59d12fe92fc7719
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-11-25 19:21:08 -0800

    built & style 404 page

--- client/app/pages/404/index.js
+++ client/app/pages/404/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+
 const page404 = () => {
-  return <div>Not Found</div>;
+  return (
+    <div className="page404">
+      <Typography noWrap variant="body1">
+        The page you are looking for was not found{' '}
+        <Link to="/">Back To Home Page</Link>
+      </Typography>
+    </div>
+  );
 };
 
 export default page404;
