fcca6ec50cfe2f42bf68ff188f0a992bc572c128
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-12-17 20:05:17 -0800

    add babel cache api

--- babel.config.js
+++ babel.config.js
@@ -1,46 +1,49 @@
-module.exports = {
-  ignore: ['server/**/*.test.js', 'client/**/*.test.js'],
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: ['@babel/transform-runtime'],
-  env: {
-    development: {
-      plugins: [
-        [
-          'module-resolver',
-          {
-            root: ['.'],
-            alias: {
-              'webpack.config': './webpack.config',
+module.exports = api => {
+  api.cache.using(() => process.env.NODE_ENV);
+  return {
+    ignore: ['server/**/*.test.js', 'client/**/*.test.js'],
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins: ['@babel/transform-runtime'],
+    env: {
+      development: {
+        plugins: [
+          [
+            'module-resolver',
+            {
+              root: ['.'],
+              alias: {
+                'webpack.config': './webpack.config',
+              },
             },
-          },
+          ],
         ],
-      ],
-    },
-    production: {
-      plugins: [
-        [
-          'module-resolver',
-          {
-            root: ['.'],
-            alias: {
-              'webpack.config': '../webpack.config',
+      },
+      production: {
+        plugins: [
+          [
+            'module-resolver',
+            {
+              root: ['.'],
+              alias: {
+                'webpack.config': '../webpack.config',
+              },
             },
-          },
+          ],
         ],
-      ],
-    },
-    test: {
-      plugins: [
-        [
-          'module-resolver',
-          {
-            root: ['.'],
-            alias: {
-              'webpack.config': './webpack.config',
+      },
+      test: {
+        plugins: [
+          [
+            'module-resolver',
+            {
+              root: ['.'],
+              alias: {
+                'webpack.config': './webpack.config',
+              },
             },
-          },
+          ],
         ],
-      ],
+      },
     },
-  },
+  };
 };
